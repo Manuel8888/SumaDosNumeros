490124d64ef5ebc1e45db4a56358c026ea8c62eb
Manuel <Manueladalberto405@gmail.com>
2024-08-30 13:56:04 -0600

    Agregar archivos de proyecto.

---
+++ SumaDosNumeros/Models/CalcularNumeros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDosNumeros.Models
+{
+    public class CalcularNumeros
+    {
+
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+        public double Result => N1 + N2;
+
+    }
+}
---
+++ SumaDosNumeros/ViewModels/SumarNumeros.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SumaDosNumeros.Models;
+
+namespace SumaDosNumeros.ViewModels
+{
+    public class SumarNumeros : INotifyPropertyChanged
+    {
+        private readonly CalcularNumeros _calcular = new CalcularNumeros();
+
+        public double N1
+        {
+            get => _calcular.N1;
+            set
+            {
+                if (_calcular.N1 != value)
+                {
+                    _calcular.N1 = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public double N2
+        {
+            get => _calcular.N2;
+            set
+            {
+                if (_calcular.N2 != value)
+                {
+                    _calcular.N2 = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public double Result => _calcular.Result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    } 
+}
